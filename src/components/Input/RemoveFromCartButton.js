7312369f681ce35dfd61d986953b56682c0ef450
Lemuel <sirlemuel@Waynes-MacBook-Air.local>
2018-07-31 23:25:57 -0400

    refactor remove from cart btn

--- src/components/Input/RemoveFromCartButton.js
+++ src/components/Input/RemoveFromCartButton.js
@@ -3,29 +3,15 @@ import { connect } from 'react-redux'
 import { removeFromCart } from '../../modules/cart/actions'
 import PropTypes from 'prop-types'
 
-class RemoveFromCartButton extends Component {
-  constructor(props) {
-    super(props)
-
-    this.handleRemoveFromCart = this.handleRemoveFromCart.bind(this)
-  }
-
-  handleRemoveFromCart(event, productId) {
-    this.props.removeFromCart(productId)
-  }
-
-  render() {
-    const { productId } = this.props
-
-    return (
-      <input
-        type="button"
-        className="car-item__remove-btn btn btn-warning"
-        value="Remove"
-        onClick={() => this.handleRemoveFromCart(null, productId)}
-      />
-    )
-  }
+const RemoveFromCartButton = ({ productId, removeFromCart }) => {
+  return (
+    <input
+      type="button"
+      className="car-item__remove-btn btn btn-warning"
+      value="Remove"
+      onClick={() => removeFromCart(productId)}
+    />
+  )
 }
 
 
